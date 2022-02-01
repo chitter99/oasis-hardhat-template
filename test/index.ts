9d977b9f11e96b0ac780d3ab1eeca1fe8d3ea4e5
Aaron Schmid <aschmid@mysmartheart.ch>
2022-02-01 16:04:50 +0100

    Added tests for nft

--- test/index.ts
+++ test/index.ts
@@ -58,3 +58,38 @@ describe("WrapperCoolToken", function () {
 		expect(amount).to.equal(await token.balanceOf(receiver.address));
 	});
 });
+
+describe("CoolNFT", function () {
+	let Token: et.ContractFactory, CoolNFT: et.ContractFactory;
+
+	before(async function() {
+		Token = await ethers.getContractFactory("CoolToken");
+		CoolNFT = await ethers.getContractFactory("CoolNFT");
+	});
+
+	it("Mint a nft by depositing cool token and withdraw that tokens by burning the nft", async function() {
+		const [owner, receiver] = await ethers.getSigners();
+		const amount = ethers.utils.parseUnits("1");
+		const nftId = 1;
+		const token = await Token.deploy(amount);
+		const nft = await CoolNFT.deploy(token.address);
+
+		// Mint nft
+		await token.approve(nft.address, amount);
+		let tx = await nft.deposit(amount);
+		
+		await expect(tx).to.emit(nft, "Transfer").withArgs(ethers.constants.AddressZero, owner.address, nftId);
+		expect(1).to.equal(await nft.balanceOf(owner.address));
+		expect(amount).to.equal(await token.balanceOf(nft.address));
+		expect(await nft.tokenBalanceOf(nftId)).to.equal(amount);
+		
+		// Transfer nft to reciver
+		await nft.transferFrom(owner.address, receiver.address, nftId);
+
+		// Burn nft
+		tx = await nft.connect(receiver).withdraw(nftId);
+		
+		await expect(tx).to.emit(nft, "Transfer").withArgs(receiver.address, ethers.constants.AddressZero, nftId);
+		expect(amount).to.equal(await token.balanceOf(receiver.address));
+	});
+});
